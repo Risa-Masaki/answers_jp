331997a765f13b8a94d0fe088155b2fa8275c13f
RIsa-Masaki <risa.masaki.7227@gmail.com>
2017-11-26 17:31:10 +0900

    編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
   def edit
 
   end
+
+  def update
+    current_user.update(update_params)
+    redirect_to root_path
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:email, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :questions do
     resources :answers, only: [:create]
   end
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
