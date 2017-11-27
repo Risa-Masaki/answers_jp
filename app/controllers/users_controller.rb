4cfbeef5ec4e9c2e3be6761b63439f5d2b4e6d1c
RIsa-Masaki <risa.masaki.7227@gmail.com>
2017-11-27 11:05:55 +0900

    画像登録機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
 
   def update
     current_user.update(update_params)
-    redirect_to root_path
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126062421) do
+ActiveRecord::Schema.define(version: 20171127015421) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 20171126062421) do
     t.text     "availability",           limit: 65535
     t.text     "interest",               limit: 65535
     t.text     "introduction",           limit: 65535
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
+    t.datetime "avatar_updated_at"
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
